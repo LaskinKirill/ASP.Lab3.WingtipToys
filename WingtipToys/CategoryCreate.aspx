2c3733d9595be31baab09734c66b6f10bd06fbc7
Kirill Laskin <KirillLaskin@itcenterifmo.onmicrosoft.com>
2021-04-23 18:28:35 +0300

    Добавьте файлы проекта.

---
+++ WingtipToys/CategoryCreate.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CategoryCreate.aspx.cs" Inherits="WingtipToys.CategoryCreate" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+<h1>Add new product</h1>
+    <div>
+        
+        <asp:Label ID="LabelName" runat="server" AssociatedControlID="Name" Text="Name:"></asp:Label>
+        <br />
+        <asp:TextBox ID="Name" runat="server" Width="159px"></asp:TextBox>
+&nbsp;&nbsp;
+        <asp:RequiredFieldValidator ID="NameValidator" runat="server" ControlToValidate="Name" ErrorMessage="Category name is required" Display="Dynamic"></asp:RequiredFieldValidator>
+        <br />
+        <asp:Label ID="LabelPrice" runat="server" AssociatedControlID="Price" Text="Price:"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server" Width="157px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="LabelCategory" runat="server" AssociatedControlID="Category" Text="Category:"></asp:Label>
+&nbsp;
+        <textarea id="TextAreaCategory" cols="20" name="S2" rows="2"></textarea><br />
+        <br />
+        <asp:Label ID="LabelDescription" runat="server" AssociatedControlID="Description" Text="Description:"></asp:Label>
+        <br />
+        <textarea id="Description" cols="20" name="S1" ></textarea>&nbsp;
+        <asp:RegularExpressionValidator ID="DescriptionLengthValidator" runat="server" ControlToValidate="Description" ErrorMessage="Description should be maximum 500 symbols long" ValidationExpression="^.{0,500}$"></asp:RegularExpressionValidator>
+        <br />
+        <asp:Label ID="LabelProductimageURL" runat="server" AssociatedControlID="ProductimageURL" Text="Product image URL:"></asp:Label>
+        <br />
+        <asp:TextBox ID="TextBox4" runat="server" Width="159px"></asp:TextBox>
+        <br />
+        <asp:Button ID="CreateProduct" runat="server" Text="Create product" Width="113px" OnClick="CreateProduct_Click" />
+        <br />
+        <br />
+        <br />
+        <asp:ValidationSummary ID="ValidationSummary1" runat="server" />
+    </div>
+
+</asp:Content>
---
+++ WingtipToys/CategoryCreate.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторной генерации кода. 
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace WingtipToys
+{
+
+
+    public partial class CategoryCreate
+    {
+
+        /// <summary>
+        /// LabelName элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelName;
+
+        /// <summary>
+        /// Name элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Name;
+
+        /// <summary>
+        /// NameValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator NameValidator;
+
+        /// <summary>
+        /// LabelPrice элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelPrice;
+
+        /// <summary>
+        /// TextBox2 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// LabelCategory элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelCategory;
+
+        /// <summary>
+        /// LabelDescription элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelDescription;
+
+        /// <summary>
+        /// DescriptionLengthValidator элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator DescriptionLengthValidator;
+
+        /// <summary>
+        /// LabelProductimageURL элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelProductimageURL;
+
+        /// <summary>
+        /// TextBox4 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// CreateProduct элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button CreateProduct;
+
+        /// <summary>
+        /// ValidationSummary1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+    }
+}
